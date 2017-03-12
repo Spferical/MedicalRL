427c428544ab42a0c1799a2a6130986c024fc185
Matthew Pfeiffer <spferical@gmail.com>
2017-03-12 00:29:23 -0500

    Allow colors in message event

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import tcod
 
 
 class EventType(Enum):
@@ -53,8 +54,9 @@ class EventHandler(object):
                 return True
         return False
 
+
+def message(message, color=tcod.white):
+    events.send(Event(EventType.MESSAGE, (message, color)))
+
+
 events = EventHandler()
-
-
-def message(message):
-    events.send(Event(EventType.MESSAGE, message))
